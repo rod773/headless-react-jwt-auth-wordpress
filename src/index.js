da567edf996c51c5030a86398635b3229f514582
rod773 <104315743+rod773@users.noreply.github.com>
2024-01-29 21:27:32 +0100

    create root

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import appService from "./authentication";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(
-  <App appService={appService} />,
-  document.getElementById("root")
-);
+const domNode = document.getElementById("root");
+const root = createRoot(domNode);
+
+root.render(<App appService={appService} />, document.getElementById("root"));
